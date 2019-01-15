15bce3bfb2c48b4b9201feaff7daf66eb4af9b8c
Greg Brown <grgbrn@gmail.com>
2019-01-15 18:02:29 +0100

    convert example to typescript

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,13 +1,13 @@
-// Demonstration of multiple force acting on 
+// Demonstration of multiple force acting on
 // bodies (Mover class)
 // Bodies experience gravity continuously
 // Bodies experience fluid resistance when in "water"
 
 // Five moving bodies
-var movers = [];
+let movers: Mover[] = [];
 
 // Liquid
-var liquid;
+var liquid: Liquid;
 
 function setup() {
   createCanvas(640, 360);
@@ -57,87 +57,89 @@ function reset() {
   }
 }
 
-var Liquid = function(x, y, w, h, c) {
-  this.x = x;
-  this.y = y;
-  this.w = w;
-  this.h = h;
-  this.c = c;
-};
-  
-// Is the Mover in the Liquid?
-Liquid.prototype.contains = function(m) {
-  var l = m.position;
-  return l.x > this.x && l.x < this.x + this.w &&
-         l.y > this.y && l.y < this.y + this.h;
-};
-  
-// Calculate drag force
-Liquid.prototype.calculateDrag = function(m) {
-  // Magnitude is coefficient * speed squared
-  var speed = m.velocity.mag();
-  var dragMagnitude = this.c * speed * speed;
+class Liquid {
 
-  // Direction is inverse of velocity
-  var dragForce = m.velocity.copy();
-  dragForce.mult(-1);
-  
-  // Scale according to magnitude
-  // dragForce.setMag(dragMagnitude);
-  dragForce.normalize();
-  dragForce.mult(dragMagnitude);
-  return dragForce;
-};
-  
-Liquid.prototype.display = function() {
-  noStroke();
-  fill(50);
-  rect(this.x, this.y, this.w, this.h);
-};
+  constructor(
+    public x: number,
+    public y: number,
+    public w: number,
+    public h: number,
+    public c: number) {
+      // nothing to do because of ts magic!
+    }
 
-function Mover(m,x,y) {
-  this.mass = m;
-  this.position = createVector(x,y);
-  this.velocity = createVector(0,0);
-  this.acceleration = createVector(0,0);
+    // is the Mover in the liquid?
+    contains(m: Mover): boolean {
+      const l = m.position;
+      return l.x > this.x && l.x < this.x + this.w &&
+             l.y > this.y && l.y < this.y + this.h;
+    }
+
+    calculateDrag(m: Mover): p5.Vector {
+      var speed = m.velocity.mag();
+      var dragMagnitude = this.c * speed * speed;
+    
+      // Direction is inverse of velocity
+      var dragForce = m.velocity.copy();
+      dragForce.mult(-1);
+      
+      // Scale according to magnitude
+      // dragForce.setMag(dragMagnitude);
+      dragForce.normalize();
+      dragForce.mult(dragMagnitude);
+      return dragForce;
+    }
+
+    display() {
+      noStroke();
+      fill(50);
+      rect(this.x, this.y, this.w, this.h);
+    }
 }
 
-// Newton's 2nd law: F = M * A
-// or A = F / M
-Mover.prototype.applyForce = function(force) {
-  var f = p5.Vector.div(force,this.mass);
-  this.acceleration.add(f);
-};
-  
-Mover.prototype.update = function() {
-  // Velocity changes according to acceleration
-  this.velocity.add(this.acceleration);
-  // position changes by velocity
-  this.position.add(this.velocity);
-  // We must clear acceleration each frame
-  this.acceleration.mult(0);
-};
+class Mover {
+  x: number
+  y: number
+  mass: number
+  position: p5.Vector
+  velocity: p5.Vector
+  acceleration: p5.Vector
 
-Mover.prototype.display = function() {
-  stroke(0);
-  strokeWeight(2);
-  fill(255,127);
-  ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
-};
-
-// Bounce off bottom of window
-Mover.prototype.checkEdges = function() {
-  if (this.position.y > (height - this.mass*8)) {
-    // A little dampening when hitting the bottom
-    this.velocity.y *= -0.9;
-    this.position.y = (height - this.mass*8);
+  constructor(m: number, x: number, y: number) {
+    this.mass = m
+    this.x = x
+    this.y = y
+    this.position = createVector(x,y);
+    this.velocity = createVector(0,0);
+    this.acceleration = createVector(0,0);
   }
-};
 
+  applyForce(force: p5.Vector) {
+    var f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
+  }
 
+  update() {
+    // Velocity changes according to acceleration
+    this.velocity.add(this.acceleration);
+    // position changes by velocity
+    this.position.add(this.velocity);
+    // We must clear acceleration each frame
+    this.acceleration.mult(0);
+  }
 
+  display() {
+    stroke(0);
+    strokeWeight(2);
+    fill(255,127);
+    ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
+  }
 
-
-
-
-
+  checkEdges() {
+    if (this.position.y > (height - this.mass*8)) {
+      // A little dampening when hitting the bottom
+      this.velocity.y *= -0.9;
+      this.position.y = (height - this.mass*8);
+    }
+  }
+}
